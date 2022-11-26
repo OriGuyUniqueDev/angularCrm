66f950c00c88097b9e4cae91dfbf9d97da9f493b
OriGuyUniqueDev <origuydev@gmail.com>
2022-11-26 19:10:53 +0200

    .

--- src/app/components/customer-card-mobile/customer-card-mobile.component.ts
+++ src/app/components/customer-card-mobile/customer-card-mobile.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-card-mobile.component.css']
 })
 export class CustomerCardMobileComponent implements OnInit {
-
+name:string = 'Ori Guy'
+tel:string = '0547520899'
+email:string = 'origuy2021@gmail.com'
+showEditMenu:boolean = false
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/customer-dashboard/customer-dashboard.component.ts
+++ src/app/components/customer-dashboard/customer-dashboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDashboardComponent implements OnInit {
   name:string = "Ori Guy"
+  showEditMenu:boolean = false
   constructor() { }
 
   ngOnInit(): void {
