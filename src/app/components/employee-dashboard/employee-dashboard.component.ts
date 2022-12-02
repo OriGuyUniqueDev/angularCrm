48ef82230d7ea6a6eef0b7646150de460753ae22
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-02 17:48:10 +0200

    basic employee

--- src/app/components/employee-dashboard/employee-dashboard.component.ts
+++ src/app/components/employee-dashboard/employee-dashboard.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger,state,style,animate,transition,} from '@angular/animations';
 
 @Component({
   selector: 'app-employee-dashboard',
   templateUrl: './employee-dashboard.component.html',
-  styleUrls: ['./employee-dashboard.component.css']
+  styleUrls: ['./employee-dashboard.component.css'],
+  animations: [
+    trigger('fade',[
+      transition(':enter',[
+        style({opacity:0}),
+        animate('0.25s',style({opacity:1}))
+      ]),
+      transition(':leave',[
+        style({opacity:1}),
+        animate('0.5s',style({opacity:0}))
+      ]),
+    ])
+  ]
 })
 export class EmployeeDashboardComponent implements OnInit {
-
+  name: string = sessionStorage.getItem('email') as string;
   constructor() { }
 
   ngOnInit(): void {
