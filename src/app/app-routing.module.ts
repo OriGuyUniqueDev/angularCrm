f05d7e8763c533de0e4a61b1c320dca7f02f8ba3
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-03 08:38:01 +0200

    oreder in component folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
-import { CustomerPageComponent } from './components/customer-page/customer-page.component';
-import { EmployeeDashboardComponent } from './components/employee-dashboard/employee-dashboard.component';
-import { LoginComponent } from './components/login/login.component';
+import { CustomerDashboardComponent } from './components/customersComponents/customer-dashboard/customer-dashboard.component';
+import { CustomerPageComponent } from './components/customersComponents/customer-page/customer-page.component';
+import { EmployeeDashboardComponent } from './components/employeeComponents/employee-dashboard/employee-dashboard.component';
+import { LoginComponent } from './components/loginResister/login/login.component';
 import { PnfComponent } from './components/pnf/pnf.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/loginResister/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './components/login/login.component';
-import { CustomerDashboardComponent } from './components/customer-dashboard/customer-dashboard.component';
-import { EmployeeDashboardComponent } from './components/employee-dashboard/employee-dashboard.component';
+import { LoginComponent } from './components/loginResister/login/login.component';
+import { CustomerDashboardComponent } from './components/customersComponents/customer-dashboard/customer-dashboard.component';
+import { EmployeeDashboardComponent } from './components/employeeComponents/employee-dashboard/employee-dashboard.component';
 import { PnfComponent } from './components/pnf/pnf.component';
 import { FormsModule } from '@angular/forms';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { CustomerCardMobileComponent } from './components/customer-card-mobile/customer-card-mobile.component';
-import { OffCanvasComponent } from './components/off-canvas/off-canvas.component';
+import { LoginFormComponent } from './components/loginResister/login-form/login-form.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { CustomerCardMobileComponent } from './components/customersComponents/customer-card-mobile/customer-card-mobile.component';
+import { OffCanvasComponent } from './components/navigation/off-canvas/off-canvas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './components/register/register.component';
-import { AddCustomerComponent } from './components/add-customer/add-customer.component';
-import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { RegisterComponent } from './components/loginResister/register/register.component';
+import { AddCustomerComponent } from './components/customersComponents/add-customer/add-customer.component';
+import { EditCustomerComponent } from './components/customersComponents/edit-customer/edit-customer.component';
 import { AllFilterPipe } from './pipes/all-filter.pipe';
-import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { CustomerPageComponent } from './components/customersComponents/customer-page/customer-page.component';
+
 
 
 
