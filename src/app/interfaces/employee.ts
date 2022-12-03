742b138c596f8363ddd8e18a11817d4b6d737a0a
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-03 08:41:00 +0200

    employee interface

--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -1,2 +1,11 @@
 export interface Employee {
+    dateOfBirth:Date,
+    email:string,
+    firstname:string,
+    lastname:string,
+    managerOf:string[],
+    moto:string,
+    phone:string,
+    profileImg:string,
+    role:string
 }
