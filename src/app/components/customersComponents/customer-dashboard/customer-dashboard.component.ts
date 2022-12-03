75f6846d7f2901f5c7de63493a91ffeab75fa829
OriGuyUniqueDev <origuydev@gmail.com>
2022-12-03 20:52:31 +0200

    .

--- src/app/components/customersComponents/customer-dashboard/customer-dashboard.component.ts
+++ src/app/components/customersComponents/customer-dashboard/customer-dashboard.component.ts
@@ -2,9 +2,10 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Customer } from 'src/app/interfaces/customer';
 import { CustomerService } from 'src/app/services/customer.service';
 import {trigger,state,style,animate,transition,} from '@angular/animations';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddCustomerComponent } from '../add-customer/add-customer.component';
 import { filter } from 'rxjs';
+import { EditCustomerComponent } from '../edit-customer/edit-customer.component';
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -28,6 +29,7 @@ export class CustomerDashboardComponent implements OnInit {
   showEditMenu: boolean = false;
   showLoader: boolean = true;
   customerArr!: Customer[];
+  isAdded:boolean = false
   search:string = ''
   innerWidth: any;
   filterSearch:keyof Customer = 'firstname'
@@ -57,4 +59,12 @@ export class CustomerDashboardComponent implements OnInit {
   radioChange(event:any){
     this.filterSearch = event.target.defaultValue
   }
+    editCustomer(customer:Customer){
+      const modalRef = this.modal.open(EditCustomerComponent,{
+        size:'lg',
+        centered:true,
+        windowClass:'dark-modal'
+      })
+      modalRef.componentInstance.id = customer.id
+    }
 }
